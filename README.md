775b234944aa2181c6c032f2c174991ab87ca393
wiki-Bird <41028518+wiki-Bird@users.noreply.github.com>
2024-08-28 17:21:37 +1200

    Updated to have embeds and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bluescreen-web
+# web redirects
+
+create one URL and redirect based on browser. 
+
+useful for browser extensions (firefox, chrome, etc). also embeds content on social media (eg: discord embeds, twitter cards, etc).
+
+---
+
+current branches:
+
+- [bluescreen](https://github.com/wiki-Bird/web-redirects/tree/bluescreen)
--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function checkBrowser() {
     const userAgent = navigator.userAgent.toLowerCase();
     
     if (userAgent.indexOf("firefox") > -1) {
-        window.location.href = "firefox.html";
+        window.location.href = "#firefox";
     }
     else if (userAgent.indexOf("chrome") > -1 || userAgent.indexOf("edge") > -1 || userAgent.indexOf("opera") > -1) {
-        window.location.href = "chrome.html";
+        window.location.href = "#chrome";
     }
     else {
-        window.location.href = "default.html";
+        window.location.href = "#default";
     }
 }
